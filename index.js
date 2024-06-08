4e6981567a71b0c02c4a4ba9dba57d3f731468ea
Alejandro esquer caraveo <alexcaraveo@outlook.com>
2024-06-08 02:43:52 -0700

    added UI and event handling logic

--- index.js
+++ index.js
@@ -1,71 +1,48 @@
-const map = new Map();
-
-map.set(1, "piedra");
-map.set(2, "papel");
-map.set(3, "tijera");
-
-
+let btnContainer = document.querySelector(".container");
+let results = document.querySelector(".results");
 
 function getComputerChoice() {
     let num = Math.random();
 
     if(num < 0.33333) {
-        return map.get(1);
+        return "piedra";
     }
     else if (num < 0.66666) {
-        return map.get(2);
+        return "papel";
 
     } 
     else {
-        return map.get(3);
+        return "tijera";
     }  
 }
 
-function getHumanChoice() {
-    let choice = prompt("1. priedra \n2. papel \n3. tijera");
-
-    return map.get(+choice);
-}
-
-
-
-
 function playGame () {
-
     let humanScore = 0;
     let computerScore = 0;
-    let roundsPlayed = 0;
-
+    
     function playRound(humanChoice, computerChoice) {
         if(humanChoice == computerChoice) {
-            console.log("empate");
+            results.textContent = "empate";
         } else if (
             (humanChoice == "tijera" && computerChoice == "papel") ||
             (humanChoice == "papel" && computerChoice == "piedra") ||
             (humanChoice == "piedra" && computerChoice == "tijera")
         ) {
-            console.log(`Ganaste: ${humanChoice} vence a ${computerChoice}`);
+            results.textContent = `Ganaste: ${humanChoice} vence a ${computerChoice}`;
             humanScore++;
         }
         else {
-            console.log(`Perdiste: ${computerChoice} vence a ${humanChoice}`);
+            results.textContent = `Perdiste: ${computerChoice} vence a ${humanChoice}`;
             computerScore++;
         }
     }
 
-    while(roundsPlayed < 5) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-        roundsPlayed++;
-    }
-    
-    console.log(`Human socore: ${humanScore}. Computer score: ${computerScore}`);
+    btnContainer.addEventListener("click", (event) => {
+        let humanChoice = event.target.id;
+        playRound(humanChoice, getComputerChoice());
+    });
 }
 
 playGame();
 
 
-
-
-//        console.log(`Putos humanos: ${humanScore}. Puntos computadora: ${computerScore}` )
